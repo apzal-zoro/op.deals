51741d8524b33f348a688fc2c640835acc492bdd
Apzal Rahman A <apzal5903@gmail.com>
2025-06-28 02:12:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,15 +1,28 @@
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
 import { Switch } from '@/components/ui/switch';
 import { Label } from './ui/label';
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function ThemeToggle() {
+  const [mounted, setMounted] = React.useState(false);
   const { setTheme, theme } = useTheme();
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="flex items-center space-x-2">
+        <Label htmlFor="dark-mode-switch" className="text-sm font-normal">Dark</Label>
+        <Skeleton className="h-6 w-11 rounded-full" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center space-x-2">
