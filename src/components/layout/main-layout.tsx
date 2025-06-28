a46e2a56a729ea2902f3cf79aae47fee9bab7daa
Apzal Rahman A <apzal5903@gmail.com>
2025-06-28 07:05:43 +0000

    whats the plan about the ad thing we spoke early on

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -18,16 +18,6 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
     <div className="flex flex-col min-h-screen">
       <Header />
 
-      {/* This new container holds the ad. It is outside the 3-column structure. */}
-      {showTopAd && (
-        <div className="w-full flex justify-center pt-6">
-            {/* This inner div constrains the ad to the content width */}
-            <div className="w-full max-w-[1200px]">
-                <TopAdPlaceholder />
-            </div>
-        </div>
-      )}
-
       <div className="w-full flex-grow flex justify-center">
         <div className="flex w-full max-w-screen-2xl px-6 gap-6">
           <aside className="hidden lg:block w-[160px] shrink-0 pt-6">
@@ -35,10 +25,17 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
               <AdPlaceholder side="left" />
             </div>
           </aside>
+          
           <main className="w-full min-w-0 max-w-[1200px] flex-grow pt-6 pb-12">
+            {showTopAd && (
+              <div className="mb-6">
+                <TopAdPlaceholder />
+              </div>
+            )}
             {showPlatformSelector && <PlatformSelector />}
             {children}
           </main>
+
           <aside className="hidden lg:block w-[160px] shrink-0 pt-6">
             <div className="sticky top-20">
               <AdPlaceholder side="right" />
